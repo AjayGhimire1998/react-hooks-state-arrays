85e7fea55a09a4a6f8cc5d32bdbceacfcac6b1d4
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-29 11:29:54 +1100

    done with the lab

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
-import { spicyFoods, getNewSpicyFood } from "../data";
+import React from "react";
+import { useState } from "react/cjs/react.development";
+import { spicyFoods, getNewSpicyFood } from "../data/index";
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+  const [foods, setFoods]  = useState(spicyFoods);
+
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArr = [...foods, newFood];
+    setFoods(newFoodArr)
   }
+  function handleLiClick(id) {
+    // const newFoodArray = foods.filter((food) => food.id !== id);
+    // setFoods(newFoodArray);
+
+    const newFoodArray = foods.map((food) => {
+      if( food.id === id){
+        return{
+          ...food,
+          heatLevel: food.heatLevel +1
+        }
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ));
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{foodList}</ul>
     </div>
   );
 }
